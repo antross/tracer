7798c16b48d7f8c1e23628631132f5fb3af57b92
Tony Ross <antross@gmail.com>
2018-07-13 12:03:44 -0500

    Omit args to `clearTimeout` and `clearInterval` from trace
    Reduces noise in the same way as omitting the result from `setTimeout` and `setInterval`.

--- src/suppress/setInterval.js
+++ src/suppress/setInterval.js
@@ -6,18 +6,28 @@ import { handle } from '../watch.js';
  * Map traces for `setInterval` to omit the returned timer ID.
  * This normalizes Firefox traces since it starts at `2` instead of `1`.
  * It also reduces noise due to extra `setInterval` calls between traces.
+ * Similarly, the ID passed to `clearInterval` is omitted.
  */
 export default function suppress() {
 
-    const key = 'setInterval';
+    handle('clearInterval', {
+        apply: (target, obj, args) => {
+            const result = Reflect.apply(target, obj, args);
 
-    handle(key, {
+            // Omit the arguments to exclude the ID from the trace.
+            new Trace().apply(obj, 'clearInterval', []);
+
+            return result;
+        }
+    });
+
+    handle('setInterval', {
         apply: (target, obj, args) => {
             const result = Reflect.apply(target, obj, args);
 
             // Omit the `result` parameter to set it to `undefined`.
             // This causes the result to be excluded from the trace.
-            new Trace().apply(obj, key, args);
+            new Trace().apply(obj, 'setInterval', args);
 
             return result;
         }
--- src/suppress/setTimeout.js
+++ src/suppress/setTimeout.js
@@ -6,18 +6,28 @@ import { handle } from '../watch.js';
  * Map traces for `setTimeout` to omit the returned timer ID.
  * This normalizes Firefox traces since it starts at `2` instead of `1`.
  * It also reduces noise due to extra `setTimeout` calls between traces.
+ * Similarly, the ID passed to `clearTimeout` is omitted.
  */
 export default function suppress() {
 
-    const key = 'setTimeout';
+    handle('clearTimeout', {
+        apply: (target, obj, args) => {
+            const result = Reflect.apply(target, obj, args);
 
-    handle(key, {
+            // Omit the arguments to exclude the ID from the trace.
+            new Trace().apply(obj, 'clearTimeout', []);
+
+            return result;
+        }
+    });
+
+    handle('setTimeout', {
         apply: (target, obj, args) => {
             const result = Reflect.apply(target, obj, args);
 
             // Omit the `result` parameter to set it to `undefined`.
             // This causes the result to be excluded from the trace.
-            new Trace().apply(obj, key, args);
+            new Trace().apply(obj, 'setTimeout', args);
 
             return result;
         }
