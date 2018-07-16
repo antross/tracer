3c91871ce3f2ef5591908d67fadf995603b1d33f
Tony Ross <antross@gmail.com>
2018-07-16 13:36:00 -0500

    Add icons to show when tracing is on/off

--- dist/background-script.js
+++ dist/background-script.js
@@ -6,13 +6,25 @@ const enabledTabs = new Set();
 browser.tabs.onActivated.addListener(info => {
     const tabId = info.tabId;
     const enabled = enabledTabs.has(tabId);
-    browser.browserAction.setBadgeText(enabled ? '!' : '');
+    updateIcon(enabled);
 });
 
 browser.browserAction.onClicked.addListener(tab => {
     const tabId = tab.id;
     const enabled = !enabledTabs.has(tabId);
-    if (enabled) enabledTabs.add(tabId);
-    else enabledTabs.delete(tabId);
+    if (enabled) {
+        enabledTabs.add(tabId);
+    } else {
+        enabledTabs.delete(tabId);
+    }
+    updateIcon(enabled);
     browser.tabs.sendMessage(tabId, { enabled: enabled });
 });
+
+function updateIcon(enabled) {
+    browser.browserAction.setIcon({
+        path: {
+            "40": `image/${enabled ? 'tracing' : 'default'}.png`
+        }
+    });
+}
