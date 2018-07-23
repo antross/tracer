5d64e51341d2b24dff2050ce1f684e603c425daf
Tony Ross <antross@gmail.com>
2018-07-23 12:23:51 -0500

    Suppress sub-calls from `String.prototype.split`
    Also improved documentation for other ignored calls on `String.prototype`.

--- src/suppress/String.js
+++ src/suppress/String.js
@@ -11,7 +11,59 @@ const ignoreSubCalls = _ignoreSubCalls;
  */
 export default function suppress() {
 
+    /**
+     * Calling `String.prototype.match` calls `global` and `exec` in Chrome
+     * and Firefox, but not Edge. Ignoring to reduce noise.
+     * 
+     * Code: `'11px'.match(/\d+px/);`
+     * 
+     * Trace:
+     * ```
+     * "11px".match({...}) === [...];
+     * {...}.global === false;
+     * [...] = {...}.exec("11px");
+     * ```
+     */
     ignoreSubCalls(String.prototype, 'match');
+
+    /**
+     * Calling `String.prototype.replace` calls `global` and `exec` in Chrome
+     * and Firefox, but not Edge. Ignoring to reduce noise.
+     * 
+     * Code: `'11px'.replace(/(\d+)px/, '$1');`
+     * 
+     * Trace:
+     * ```
+     * "11px".replace({...}, "$1") === "11";
+     * {...}.global === false;
+     * [...] = {...}.exec("11px");
+     * ```
+     */
     ignoreSubCalls(String.prototype, 'replace', [1]);
+
+    /**
+     * When passed a `RegExp`, `String.prototype.split` produces a large number
+     * of sub-calls in Chrome and Firefox, but not Edge. Ignoring to reduce
+     * noise.
+     * 
+     * Code: `'12'.split(/(?=[\s\S])/);`
+     * 
+     * Trace:
+     * ```
+     * [...] = "12".split({...});
+     * {...}.flags === "";
+     * {...}.global === false;
+     * {...}.ignoreCase === false;
+     * {...}.multiline === false;
+     * {...}.dotAll === false; // Only in Chrome, not Firefox
+     * {...}.unicode === false;
+     * {...}.sticky === false;
+     * {...} = new RegExp({...}, "y");
+     * [...] = {...}.exec("12");
+     * [...] = {...}.exec("12");
+     * [...] = {...}.exec("12");
+     * ```
+     */
+    ignoreSubCalls(String.prototype, 'split');
 
 }
