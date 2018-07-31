63034e20bb15a70c8ba2fc3bee49f32600da4b76
Tony Ross <antross@gmail.com>
2018-07-31 16:23:46 -0500

    Slowly increment stabilized dates
    This was another issue affecting #6, but still doesn't fully fix the page. One of the loaded scripts had a tight loop waiting for the date to increment (possibly as part of generating unique IDs).

--- src/stabilize/Date.js
+++ src/stabilize/Date.js
@@ -11,12 +11,26 @@ const Date_toString = Date.prototype.toString;
 let d = new NativeDate(2018, 6, 17, 11, 32).getTime();
 
 /**
- * Always return the same stable date.
- * An earlier approach stepped in 100ms increments, but produced a fair amount
+ * Track the number of times `now()` has been called to limit increments.
+ */
+let calls = 0;
+
+/**
+ * Return a stabilized date incrementing by 1ms every 100 calls.
+ * This reduces noise due to extra calls while avoiding hanging pages
+ * which use tight loops while waiting for the date to increment.
+ * An earlier approach stepped on every call, but produced a fair amount
  * of noise if an additional call was made early on in one of the traces.
  * @return {number} The next stabilized step.
  */
 export function now() {
+    calls++;
+
+    if (calls > 100) {
+        d += 1;
+        calls = 0;
+    }
+
     return d;
 }
 
